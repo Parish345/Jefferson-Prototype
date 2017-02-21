5bd571e35c3e82e4fdb63b2373e4bb37b002d135
Parish Hansen <parishhansen@gmail.com>
2017-02-21 15:43:27 -0700

    Blending mobiscroll into our project

---
+++ Jefferson/js/cipher.js
@@ -0,0 +1,118 @@
+function init() {
+    var x = document.getElementById('key').value
+    var wheel1 = ['b', 'v']
+    if (x == 1) {
+      wheel1 = [{value: 1, display:'a'}, {value: 2, display:'b'}];
+    }
+    mobiscroll.scroller('#test', {
+        theme: 'ios',
+        display: 'inline',
+        width: 25,
+        wheels: [
+            [{
+                label: 'Wheel 1',
+                data: wheel1
+            },
+            {
+                label: 'Wheel 2',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 3',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 4',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 5',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 6',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 7',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 8',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 9',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 10',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 11',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 12',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 13',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 14',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 15',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 16',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 17',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 18',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 19',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 20',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 21',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 22',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 23',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 24',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 25',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            },
+            {
+                label: 'Wheel 26',
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            }]
+        ]
+    });
+}
---
+++ Jefferson/js/scripts.js
@@ -0,0 +1,44 @@
+function openTab(evt, tabName) {
+var i, x, y, tablinks;
+x = document.getElementsByClassName("tab");
+y = document.getElementsByClassName("quiz");
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+}
+for (i = 0; i < y.length; i++) {
+    y[i].style.display = "none";
+}
+tablinks = document.getElementsByClassName("tablink");
+for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" selectedtab", "");
+}
+document.getElementById(tabName).style.display = "block";
+evt.currentTarget.className += " selectedtab";
+}
+
+function openQuiz(evt, quiz) {
+  var i;
+  x = document.getElementsByClassName("quiz");
+  y = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+  }
+  for (i = 0; i < y.length; i++) {
+      y[i].style.display = "none";
+  }
+  document.getElementById(quiz).style.display = "block";
+}
+document.getElementById('learnTab').click();
+
+function sendMessage(evt, sendMess) {
+  var i;
+  x = document.getElementsByClassName("sendMess");
+  y = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+  }
+  for (i = 0; i < y.length; i++) {
+      y[i].style.display = "none";
+  }
+  document.getElementById(sendMess).style.display = "block";
+}
